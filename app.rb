758b046e493cd0198fb47fb862bc291eef647e50
Rebecca Souza <rebeccacsouza@gmail.com>
2018-07-31 20:15:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -52,6 +52,8 @@ class App < Sinatra::Base
     "#{word1} #{word2} #{word3} #{word4} #{word5}."
   end 
   get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
   end 
   
 end
