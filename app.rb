96ef9197650a6514f95e9997aacc8853ff24c70e
Rebecca Souza <rebeccacsouza@gmail.com>
2018-07-31 19:57:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   # Izzi
-  get '/reversename/:name' do
-    @name = params[:name]
-    return @name.reverse
-  end 
+  # get '/reversename/:name' do
+  #   @name = params[:name]
+  #   return @name.reverse
+  # end 
   # Nancy
   get '/reversename/:name' do
     @name = params[:name].reverse
